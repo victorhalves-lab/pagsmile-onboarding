f436583827f600700befbcf782485755750b402a
Victor Hugo Alves <victorhalves@gmail.com>
2026-02-05 15:54:15 +0000

    File changes

--- src/components/ui/progress.jsx
+++ src/components/ui/progress.jsx
@@ -1,3 +1,4 @@
+
 "use client"
 
 import * as React from "react"
@@ -9,12 +10,12 @@ const Progress = React.forwardRef(({ className, value, ...props }, ref) => (
   <ProgressPrimitive.Root
     ref={ref}
     className={cn(
-      "relative h-2 w-full overflow-hidden rounded-full bg-primary/20",
+      "relative h-2 w-full overflow-hidden rounded-full bg-[#2bc196]/20",
       className
     )}
     {...props}>
     <ProgressPrimitive.Indicator
-      className="h-full w-full flex-1 bg-primary transition-all"
+      className="h-full w-full flex-1 bg-[#2bc196] transition-all"
       style={{ transform: `translateX(-${100 - (value || 0)}%)` }} />
   </ProgressPrimitive.Root>
 ))
--- src/components/ui/radio-group.jsx
+++ src/components/ui/radio-group.jsx
@@ -14,12 +14,12 @@ const RadioGroupItem = React.forwardRef(({ className, ...props }, ref) => {
     (<RadioGroupPrimitive.Item
       ref={ref}
       className={cn(
-        "aspect-square h-4 w-4 rounded-full border border-primary text-primary shadow focus:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
+        "aspect-square h-4 w-4 rounded-full border border-[#2bc196] text-[#2bc196] shadow focus:outline-none focus-visible:ring-1 focus-visible:ring-[#2bc196] disabled:cursor-not-allowed disabled:opacity-50",
         className
       )}
       {...props}>
       <RadioGroupPrimitive.Indicator className="flex items-center justify-center">
-        <Circle className="h-3.5 w-3.5 fill-primary" />
+        <Circle className="h-3.5 w-3.5 fill-[#2bc196]" />
       </RadioGroupPrimitive.Indicator>
     </RadioGroupPrimitive.Item>)
   );
