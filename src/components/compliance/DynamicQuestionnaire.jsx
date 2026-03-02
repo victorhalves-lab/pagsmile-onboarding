5033d6d6b1028ebe4e7eb4eece089a5996144e70
Victor Hugo Alves <victorhalves@gmail.com>
2026-03-02 18:58:21 +0000

    File changes

--- src/components/compliance/DynamicQuestionnaire.jsx
+++ src/components/compliance/DynamicQuestionnaire.jsx
@@ -14,6 +14,7 @@ import StepNavigation from './StepNavigation';
 import DynamicQuestionRenderer from './DynamicQuestionRenderer';
 import { useOnboardingAnalytics } from '../analytics/useOnboardingAnalytics';
 import { useLeadPrefill } from './useLeadPrefill';
+import { toast } from 'sonner';
 
 // Mapeamento de ícones por palavra-chave no título da pergunta
 const ICON_MAPPINGS = {
