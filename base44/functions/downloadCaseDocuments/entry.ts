9c3ae1d7711959026893b7d44f5bd1690660bf90
Victor Hugo Alves <victorhalves@gmail.com>
2026-03-19 16:31:03 +0000

    File changes

--- base44/functions/downloadCaseDocuments/entry.ts
+++ base44/functions/downloadCaseDocuments/entry.ts
@@ -1,4 +1,4 @@
-import { createClientFromRequest } from 'npm:@base44/sdk@0.8.20';
+import { createClientFromRequest } from 'npm:@base44/sdk@0.8.21';
 import JSZip from 'npm:jszip@3.10.1';
 
 Deno.serve(async (req) => {
