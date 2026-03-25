b95552617c5390a1fec7a4a5faa83b8dda31d802
Victor Hugo Alves <victorhalves@gmail.com>
2026-03-25 12:52:52 +0000

    File changes

--- src/components/leads/LeadQuestionnaireForm.jsx
+++ src/components/leads/LeadQuestionnaireForm.jsx
@@ -37,6 +37,7 @@ import CurrencyInput from './CurrencyInput';
 import PhoneInput from './PhoneInput';
 import EmailInput from './EmailInput';
 import CnaeCoherenceAlert from './CnaeCoherenceAlert';
+import SiteValidationBadge from './SiteValidationBadge';
 import { computeSilentFlags, computeLeadScore } from '@/hooks/useLeadSilentFlags';
 
 function MCCNameDisplay({ mccCode }) {
