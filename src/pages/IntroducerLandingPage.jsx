decfc5ec6d44ce0b3b519b4f65248e7dada9a5da
Victor Hugo Alves <victorhalves@gmail.com>
2026-04-03 01:46:21 +0000

    File changes

--- src/pages/IntroducerLandingPage.jsx
+++ src/pages/IntroducerLandingPage.jsx
@@ -178,7 +178,7 @@ export default function IntroducerLandingPage() {
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ duration: 0.3 }}
               >
-                <SegmentRatesTable segmentRates={activeRates} />
+                <SegmentRatesTable segmentRates={activeRates} segmentName={activeSegment} />
 
                 <motion.div
                   initial={{ opacity: 0, y: 10 }}
