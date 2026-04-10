b43930724fb5332efa52d60529d56724403a76bb
Victor Hugo Alves <victorhalves@gmail.com>
2026-04-10 00:21:00 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Toaster } from "@/components/ui/toaster"
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
